b8f2d101f2694fdddc0586d6e1b5d2237b97d9f7
Alexey Ershov <alaershov@gmail.com>
2014-08-09 16:27:09 +0700

    Initial commit - empty project created

---
+++ lab7/lab7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lab7
+//
+//  Created by Admin on 09/08/14.
+//  Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
