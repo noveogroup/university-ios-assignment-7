634c83dcafe27349d6b325b9bcd427d06aa659a0
Samohvalov Artem <samohvalovart@gmail.com>
2015-03-15 00:12:25 -0700

    Created empty project

---
+++ Lab7/Lab7/ViewController.m
@@ -0,0 +1,13 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
