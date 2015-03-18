d061d5a7c1d1f6591f2ed1affa2ac085ca53cf0a
Alexandr Ovchinnikov <Alintorius@gmail.com>
2015-03-19 00:15:05 +0600

    Project initial commit

---
+++ LabWork7/LabWork7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LabWork7
+//
+//  Created by Александр on 19.03.15.
+//  Copyright (c) 2015 Alexandr Ovchinnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
