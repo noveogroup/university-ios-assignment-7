bbc9d80c6adebeb226f62b5697eb3a12849485a9
Sukhorukov Grigory <grigoriy.sukhorukov@gmail.com>
2016-03-14 12:19:27 +0600

    Lab 7 Sukhorukov

---
+++ Lab 7/Resources/Internal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab 7
+//
+//  Created by Sukhorukov Grigory on 14.03.16.
+//  Copyright © 2016 Sukhorukov Grigory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab 7/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab 7
+//
+//  Created by Sukhorukov Grigory on 14.03.16.
+//  Copyright © 2016 Sukhorukov Grigory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
