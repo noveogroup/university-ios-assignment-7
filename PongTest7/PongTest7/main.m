1241d45eae81f7b75a1ee2b01dcfa8bf1aed3f81
Alisa Denisova <alideni@MacBook-Pro-AliDeni.local>
2016-03-14 15:59:53 +0600

    1)реализованна простейшая игра в пин понг все во ВьюКонтроллере 2)есть две ракетки и мячик, который движется в зависимости от таймера

---
+++ PongTest7/PongTest7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PongTest7
+//
+//  Created by АлиДени on 14.03.16.
+//  Copyright © 2016 Alisa Denisova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
