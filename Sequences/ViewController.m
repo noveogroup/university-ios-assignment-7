0701683d496c59648acb3d604b50633810283e2e
Saveliy Leontiev <sava0796@mail.ru>
2016-07-21 13:03:26 +0600

    Change GCD to NSOperation autoreleasepool was added

--- Sequences/ViewController.m
+++ Sequences/ViewController.m
@@ -15,12 +15,14 @@
 @property (weak, nonatomic) IBOutlet UIButton *button;
 
 @property (nonatomic, copy) void (^sequence)();
+@property (nonatomic) NSOperationQueue *queue;
 
 @end
 
 @implementation ViewController
 
-NSArray<NSNumber *> *b(NSInteger n, NSInteger j) {
+NSArray<NSNumber *> *getNumberOfSequence(NSInteger n, NSInteger j)
+{
     NSArray<NSNumber *> *f;
     NSArray<NSNumber *> *g;
     if (n == 0) {
@@ -30,44 +32,61 @@ NSArray<NSNumber *> *b(NSInteger n, NSInteger j) {
         return @[@0, @0];
     }
     else if (j > n) {
-        return b(n, j - 1);
+        return getNumberOfSequence(n, j - 1);
     }
     else {
-        f = b(n, j - 1);
-        g = b(n - j, j);
+        f = getNumberOfSequence(n, j - 1);
+        g = getNumberOfSequence(n - j, j);
         NSArray<NSNumber *> *result = @[@([f[0] integerValue]+[g[0] integerValue]),
                                         @([f[1] integerValue]+[g[1] integerValue]+[g[0] integerValue])];
         return result;
     }
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     self.label.text = @"";
-    ViewController __weak *weakSelf = self;
-    self.sequence = ^{
-        NSInteger n = [weakSelf.textField.text integerValue];
-        for (NSInteger i =1; i <= n; i++) {
-            NSInteger a = [b(i,i)[1] integerValue] - [b(i-1,i-1)[1] integerValue];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (i == 1) {
-                    weakSelf.label.text =
-                    [NSString stringWithFormat:@"%li : %ld", (long)i, (long)a];
-                }
-                else {
-                    weakSelf.label.text =
-                    [NSString stringWithFormat:@"%@\n%li : %ld",weakSelf.label.text, (long)i, (long)a];
-                }
-            });
-        }
-    };
+    self.queue = [[NSOperationQueue alloc] init];
 }
 
 
-- (IBAction)tap:(id)sender {
+- (IBAction)tap:(id)sender
+{
     [self.view endEditing:YES];
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, self.sequence);
+    
+    [self.queue cancelAllOperations];
+    [[NSOperationQueue mainQueue] cancelAllOperations];
+    
+    NSBlockOperation *countSequence = [[NSBlockOperation alloc] init];
+    
+    NSInteger n = [self.textField.text integerValue];
+    __weak typeof(self) wself = self;
+    __weak typeof(countSequence) wcountSequence = countSequence;
+    self.sequence = ^{
+        for (NSInteger i = 1; i <= n; i++) {
+            @autoreleasepool {
+                NSInteger numberOfSequence =
+                [getNumberOfSequence(i,i)[1] integerValue] - [getNumberOfSequence(i-1,i-1)[1] integerValue];
+                if (wcountSequence.isCancelled) {
+                    return;
+                }
+                [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
+                    if (i == 1) {
+                        wself.label.text =
+                        [NSString stringWithFormat:@"%li : %ld", (long)i, (long)numberOfSequence];
+                    }
+                    else {
+                        wself.label.text =
+                        [NSString stringWithFormat:@"%li : %ld\n%@", (long)i, (long)numberOfSequence, wself.label.text];
+                    }
+                }];
+            }
+        }
+    };
+    [countSequence addExecutionBlock:self.sequence];
+    
+    [self.queue addOperation:countSequence];
 }
 
 
